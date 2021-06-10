f7bcefae2508f686d9c99742c8c04e3755b346c9
wowed666 <81549835+wowed666@users.noreply.github.com>
2021-06-09 23:34:00 -0700

    Update train_module.py

--- models/train_module.py
+++ models/train_module.py
@@ -1,3 +1,32 @@
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow import keras
+
+from tensorflow.keras.preprocessing import image
+
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+from tensorflow.keras.layers.experimental.preprocessing import StringLookup
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import cv2
+
+import matplotlib.image as mpimg
+
+
+
 def group_color(styles):
     styles["colorgroup"] = -1
     styles.loc[(styles.baseColour=='Red')|
